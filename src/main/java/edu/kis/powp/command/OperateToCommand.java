bc3b6346c7d8952db5db3227142f69ca155ae1f1
Wiktor <wiktor.nowacki95@gmail.com>
2019-10-27 16:39:41 +0100

    LAB3 COMMAND ZAD1

---
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -0,0 +1,21 @@
+package edu.kis.powp.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.DriverCommand;
+
+public class OperateToCommand implements DriverCommand {
+    private int x;
+    private int y;
+    Job2dDriver job2dDriver;
+
+    public OperateToCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute() {
+        job2dDriver.operateTo(x, y);
+    }
+
+}
