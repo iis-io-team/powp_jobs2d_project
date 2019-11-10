9cc3068091ba35d8f47b9664611c4021f39785d7
Wiktor Nowacki <alterwik@MacBook-Pro-Wiktor.local>
2019-11-10 13:03:52 +0100

    fix circle printing

--- src/main/java/edu/kis/powp/factories/CircleFactory.java
+++ src/main/java/edu/kis/powp/factories/CircleFactory.java
@@ -8,17 +8,49 @@ import edu.kis.powp.jobs2d.drivers.DriverManager;
 
 public class CircleFactory implements FiguresFactory {
     private ComplexCommand complexCommand = new ComplexCommand();
+    private DriverManager driver;
 
     @Override
     public DriverCommand getShape(DriverManager driver) {
-        int r = 75;
-        complexCommand.addCommand(new SetPositionCommand(r, 0, driver.getCurrentDriver()));
-        for(int i = 0;  i < 360;  i++) {
-            int x = (int)(r * Math.cos(i));
-            int y = (int)(r * Math.sin(i));
-            complexCommand.addCommand(new OperateToCommand(x, y, driver.getCurrentDriver()));
-        }
-
+        this.driver = driver;
+        int xc = 0, yc = 0, r = 150;
+        circle(xc, yc, r);
         return complexCommand;
+    }
+
+    private void drawCircle(int xc, int yc, int x, int y)
+    {
+       positionSetOperate(xc+x, yc+y);
+       positionSetOperate(xc-x, yc+y);
+       positionSetOperate(xc+x, yc-y);
+       positionSetOperate(xc-x, yc-y);
+       positionSetOperate(xc+y, yc+x);
+       positionSetOperate(xc-y, yc+x);
+       positionSetOperate(xc+y, yc-x);
+       positionSetOperate(xc-y, yc-x);
+    }
+
+    private void positionSetOperate(int x,int y){
+        complexCommand.addCommand(new SetPositionCommand(x, y, driver.getCurrentDriver()));
+        complexCommand.addCommand(new OperateToCommand(x, y, driver.getCurrentDriver()));
+    }
+
+    private void circle(int xc, int yc, int r)
+    {
+        int x = 0, y = r;
+        int d = 3 - 2 * r;
+        drawCircle(xc, yc, x, y);
+        while (y >= x)
+        {
+            x++;
+            if (d > 0)
+            {
+                y--;
+                d = d + 4 * (x - y) + 10;
+            }
+            else
+                d = d + 4 * x + 6;
+            drawCircle(xc, yc, x, y);
+        }
     }
 }
